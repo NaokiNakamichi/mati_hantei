e495e29f9ff8b6f8d240abfb012670b1a519c8a5
Naoki Nakamichi <ronnao0516@gmail.com>
2021-04-26 17:49:42 +0900

    refa

--- generateMati.py
+++ generateMati.py
@@ -18,25 +18,29 @@ class hantei:
 
     # ４枚の牌に対して待ちを確認する。
     def check_4(self, nyuryoku):
-        a = range(1, 10)
         k = str(nyuryoku)
         tehai = []
         hora = []
         for i in k:
             tehai.append(int(i))
+        # 手牌が一種理になることはないのでその場合はPass
         if len(set(tehai)) == 1:
             return []
+        # :TODO 絶対もっとスマートな書き方がある。
         else:
-            for i in a:
+            for i in range(1, 10):
                 kari_tehai = tehai + [i]
                 if len(set(kari_tehai)) != len(kari_tehai):
                     c = collections.Counter(kari_tehai)
                     for key, value in c.items():
+                        # ここで同じ種類が2枚以上あるものについて雀頭として抜き出す。
                         if value > 1:
                             kari_tehai.remove(key)
                             kari_tehai.remove(key)
+                            # 刻子があれば和了
                             if len(set(kari_tehai)) == 1:
                                 hora.append(i)
+                            # 順子があれば和了
                             if self.renban_check(kari_tehai):
                                 hora.append(i)
                             kari_tehai.append(key)
@@ -57,12 +61,19 @@ class hantei:
         x.remove(i)
         return x
 
+    # リストから数字に変換
+    # :TODO リストから数字に変換したり文字列に変換したりとしているがそんなことやる必要がない気がする。
     def list_to_int(self, x):
         k = ""
         for j in x:
             k += str(j)
         l = int(k)
         return l
+    """
+    現在の手配から刻子もしくは順子を抜き出す。
+    全ての抜き出し方について考え、抜き出したあと全ての候補をリストにして返り値にする。
+    １３枚の手牌なら10枚の手牌の複数候補を返す。
+    """
 
     def sweap_3(self, x):
         tehai = []
@@ -84,6 +95,10 @@ class hantei:
                 sweap_list.append(self.list_to_int(k))
         return sweap_list
 
+    """
+    上記の関数を用いて主にsweap_3を使って全パターンを抜き出していきその論理和を最終の待ちとしている。
+    :TODO 七対子が判定できていない。
+    """
     def hanteikun(self, x):
         start = time.time()
         kouho_1 = self.sweap_3(x)
@@ -96,6 +111,38 @@ class hantei:
                     hora = hora | set(self.check_4(l))
         self.hantei_time = time.time() - start
         return hora
+
+class nanikiru(hantei):
+    def __init__(self):
+        super(nanikiru, self).__init__()
+
+    def maisuu_check(self,tehai, mati):
+        col = collections.Counter(list(str(tehai)))
+        maisuu = 0
+        for c in mati:
+            maisuu += (4 - col[str(c)])
+
+        return maisuu
+
+    def nanikiru_check(self,sample):
+        for k, v in sample:
+            print("手配:{}".format(k), "待ち:{}".format(v))
+            current_maisu = self.maisuu_check(k, v)
+            for tasu_i in range(1, 10):
+                if tasu_i not in v:
+                    for herasu_i in range(1, 10):
+                        tmp = list(str(k))
+                        tmp.append(str(tasu_i))
+                        col = collections.Counter(tmp)
+                        if str(herasu_i) in tmp:
+                            tmp.remove(str(herasu_i))
+                            tmp = self.list_to_int(tmp)
+                            h = hantei().hanteikun(tmp)
+                            if h:
+                                maisuu = self.maisuu_check(tmp, h)
+                                if current_maisu < maisuu:
+                                    print("自摸:{}".format(tasu_i), "切り:{}".format(herasu_i), tmp, h,
+                                          "枚数:{}".format(maisuu))
 
 
 class generate_mondai(hantei):
